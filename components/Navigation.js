ba683affee1beeb80c68b44831244bea41a1a2eb
Mommy Me <mkm0182@yahoo.com>
2023-11-20 21:07:47 -0600

    Added content to Navigation.js

---
+++ components/Navigation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../style/Navigation.css';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg" sticky="top" className="py-0.5 mt-0">
+            <Container>
+                <Navbar.Brand as={Link} to="/reacr-portfolio/">Mandy Douglas</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        {/* <Nav.Link as={Link} to="react-portfolio/home" className="fs-5">Home</Nav.Link> */}
+                        <Nav.Link as={Link} to="react-portfolio/about" className="fs-5 nav-link-highlight shadow-lg">About Me</Nav.Link>
+                        <Nav.Link as={Link} to="react-portfolio/portfolio" className="fs-5 nav-link-highlight shadow-lg">Portfolio</Nav.Link>
+                        <Nav.Link as={Link} to="react-portfolio/contact" className="fs-5 nav-link-highlight shadow-lg">Contact</Nav.Link>
+                        <Nav.Link as={Link} to="react-portfolio/resume" className="fs-5 nav-link-highlight shadow-lg">Resume</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Navigation;
